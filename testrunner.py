6af3e429204305c8d1542babe4d8ba2d90dde8b3
Ryan Liu <45245715+theryanl@users.noreply.github.com>
2020-05-30 16:42:30 -0700

    Add files via upload

---
+++ testrunner.py
@@ -0,0 +1,72 @@
+import sys
+import os
+import numpy as np
+import time
+
+start_time = time.time()
+
+my_dataset = sys.argv[1] #npz containing similarity matrix and conflict matrix
+type = int(sys.argv[2]) #type is 0 if TPMS, 1 if max min
+k = int(sys.argv[3]) #k is the upper bound for papers per reviewer
+l = int(sys.argv[4]) #l is the number of reviewers per paper
+
+Q_values = []
+Q_results = []
+AAAI_results = []
+
+if (type == 0):
+    for Q in range(10, 110, 10):
+        Q_values.append(Q/100)
+        print(f"running Q = {Q/100}...")
+        
+        #getting result for Q
+        Q_result = os.popen(f"python3 ./LP_TPMS.py {my_dataset} {Q} {k} {l}")
+        TPMS_score = float((Q_result.readlines())[-2]) #the objective value
+        Q_results.append(TPMS_score)
+        print(TPMS_score)
+        
+        #getting results for AAAI
+        summ = 0
+        
+        for i in range(10):
+            AAAI_result = os.popen(f"python3 ./AAAI_TPMS.py {my_dataset} {Q/100} {k} {l}")
+            AAAI_score = float((AAAI_result.readlines())[-2]) 
+            #the objective value
+            summ += AAAI_score
+            
+        avg = summ/10
+        AAAI_results.append(avg)
+        print(avg)
+    
+    
+elif (type == 1):
+    for Q in range(10, 110, 10):
+        Q_values.append(Q/100)
+        print(f"running Q = {Q/100}...")
+        
+        #getting result for Q
+        Q_result = os.popen(f"python3 ./LP_max_min_fairness.py {my_dataset} {Q} {k} {l}")
+        max_min_fairness = float((Q_result.readlines())[-2]) #the objective value
+        Q_results.append(max_min_fairness)
+        print(max_min_fairness)
+        
+        #getting results for AAAI
+        summ = 0
+        
+        for i in range(10):
+            AAAI_result = os.popen(f"python3 ./AAAI_max_min_fairness.py {my_dataset} {Q/100} {k} {l}")
+            AAAI_score = float((AAAI_result.readlines())[-2]) 
+            #the objective value
+            summ += AAAI_score
+            
+        avg = summ/10
+        AAAI_results.append(avg)
+        print(avg)
+        
+        
+print("Q_values:", Q_values)
+print("Q_results:", Q_results)
+print("AAAI_results:", AAAI_results)
+np.save("testrunner_results", [Q_values, Q_results, AAAI_results])
+
+print("time taken", time.time() - start_time)
