Fix computation of fairness in AAAI, add skeleton testrunnerC

count_num_institution_pairs.py
@@ -1,33 +1,29 @@
 import numpy as np
 import sys
 
-# part of this are referenced from https://github.com/xycforgithub/StrategyProof_Conference_Review
-inst_file = sys.argv[1] # file containing institution list
-assignment = sys.argv[2] #integral assignment
+def count_num_institution_pairs(assignment, inst_file):
+    inst_data = np.load(inst_file, allow_pickle=True)
+    insts = inst_data[1:] # drop number of institutions entry
+    
+    reviewers = len(insts)
 
-inst_data = np.load(inst_file, allow_pickle=True)
-insts = inst_data[1:] # drop number of institutions entry
-
-reviewers = len(insts)
-#papers = len(S[0])
-
-pairs_file = open(assignment, "r")
-count_insts = {}
-
-for pair in pairs_file.readlines():
-    tmp = pair.split() #split into paper and reviewer
-    if (len(tmp) == 1):
-        pass
-        # first line
-    else:
-        r = int(tmp[0])
-        p = int(tmp[1]) - reviewers
-        i = int(insts[r])
-        count_insts[(p, i)] += 1
-
-count = 0
-for val in count_insts.values():
-    count += 0.5 * (val) * (val - 1)
-
-pairs_file.close()
-print(count)
+    pairs_file = open(assignment, "r")
+    count_insts = {}
+    
+    for pair in pairs_file.readlines():
+        tmp = pair.split() #split into paper and reviewer
+        if (len(tmp) == 1):
+            pass
+            # first line
+        else:
+            r = int(tmp[0])
+            p = int(tmp[1]) - reviewers
+            i = int(insts[r])
+            count_insts[(p, i)] += 1
+    
+    count = 0
+    for val in count_insts.values():
+        count += 0.5 * (val) * (val - 1)
+    
+    pairs_file.close()
+    return count

testrunner.py
@@ -2,6 +2,7 @@ import sys
 import os
 import numpy as np
 import time
+from get_paper_totals import get_paper_totals
 
 start_time = time.time()
 
@@ -51,15 +52,22 @@ elif (obj_type == 1):
         print(max_min_fairness)
         
         #getting results for AAAI
-        summ = 0
-        
+        #summ = 0
+        paper_totals_list = [] 
         for i in range(10):
             AAAI_result = os.popen(f"python3 ./AAAI_max_min_fairness.py {my_dataset} {Q/100} {k} {l}")
             AAAI_score = float((AAAI_result.readlines())[-2]) 
             #the objective value
-            summ += AAAI_score
+            #summ += AAAI_score
+            #file.write(f"{name} {value}\n")
+
+            # calculate expected total similarity for each paper
+            paper_totals_list.append(get_paper_totals("output.txt", my_dataset))
+        # average together and take min
+        avg_paper_totals = sum(paper_totals_list) / 10
+        avg = np.min(avg_paper_totals)
             
-        avg = summ/10
+        #avg = summ/10
         AAAI_results.append(avg)
         print(avg)
         