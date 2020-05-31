de174221c5d54d092ce18b12aa686d0ac0deecf9
Ryan Liu <45245715+theryanl@users.noreply.github.com>
2020-05-31 15:58:41 -0700

    forgot to add this one earlier - this would be useful for calculating the max-min fairness score after BvN, however it may be susceptible to the mistake that happened earlier.

---
+++ max_min_fairness_from_assignment.py
@@ -0,0 +1,35 @@
+import numpy as np
+import sys
+
+# part of this are referenced from https://github.com/xycforgithub/StrategyProof_Conference_Review
+my_dataset = sys.argv[1] #dataset
+assignment = sys.argv[2] #integral assignment
+
+scores = np.load(my_dataset, allow_pickle=True)
+S = scores["similarity_matrix"]
+
+reviewers = len(S)
+papers = len(S[0])
+
+pairs_file = open(assignment, "r")
+
+paper_similarities = [0] * papers
+
+for pair in pairs_file.readlines():
+    tmp = pair.split() #split into paper and reviewer
+    if (len(tmp) == 1):
+        pass
+        # first line
+    else:
+        r = int(tmp[0])
+        p = int(tmp[1]) - reviewers
+        paper_similarities[p] += S[r][p]
+
+lowest = paper_similarities[0]
+
+for item in paper_similarities:
+    if (item < lowest):
+        lowest = item
+
+pairs_file.close()
+print(f'{lowest}')
