7610fc040cb69e7e01ebf6af63188972b10c4547
Steven <sjecmen@gmail.com>
2020-05-31 18:36:12 -0400

    Add generate_dataset

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,13 +1,15 @@
 import numpy as np
+import random
 
 def generate_concentrated_expertise_dataset():
     numrev = 100
     numpap = 100
     S = np.zeros((numrev, numpap))
     for i in range(numpap):
-        good_revs = np.random.choice(numrev, 6) # only 6 good per pap
+        good_revs = random.sample(range(numrev), 6) # only 6 good per pap
         for x in good_revs:
             S[x, i] = 1
+    print(S)
     M = np.zeros((numrev, numpap))
     np.savez("concentrated_expertise_dataset.npz", similarity_matrix=S.tolist(), mask_matrix=M.tolist())
 
